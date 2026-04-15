2430130d3433435fcaaf3d025bef42fdda6a3bb1
Majid <majiddl.2099@gmail.com>
2026-04-14 21:16:16 -0400

    Fix package import warning for module entrypoints

--- hiddenbridge/__init__.py
+++ hiddenbridge/__init__.py
@@ -1,3 +1,10 @@
-from hiddenbridge.experiment import run_diskann_steiner_experiment
+"""HiddenBridge package."""
+
+
+def run_diskann_steiner_experiment(*args, **kwargs):
+    from hiddenbridge.experiment import run_diskann_steiner_experiment as _run
+
+    return _run(*args, **kwargs)
+
 
 __all__ = ["run_diskann_steiner_experiment"]
